e70a106111b4486772392053efc39f51fc4d113f
Aaditya Jagdale <81344234+Contro1-cs@users.noreply.github.com>
2022-09-20 13:25:33 +0530

    Aaditya 20-09-2022

--- lib/bottomsheet/homepage.dart
+++ lib/bottomsheet/homepage.dart
@@ -37,7 +37,7 @@ class _HomepageState extends State<Homepage> {
 
   Widget getBody() {
     var w = MediaQuery.of(context).size.width;
-    var h = MediaQuery.of(context).size.height;
+    // var h = MediaQuery.of(context).size.height;
     return Column(
       children: [
         Container(
@@ -74,45 +74,18 @@ class _HomepageState extends State<Homepage> {
                   children: [
                     //Red box
                     Container(
-                      height: 84,
-                      width: w/2-23,
-                      padding: EdgeInsets.only(top: 15, left: 12) ,
-                      decoration: BoxDecoration(
-                        color: Color(0xffFCEEEE),
-                        borderRadius: BorderRadius.circular(6),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text('Pending fees', style: TextStyle(color: Color(0xff9D0709), fontSize: 14, fontWeight: FontWeight.w500),),
-                          SizedBox(height: 6,),
-                          Text('₹2,244', style: TextStyle(color: Color(0xffAC0806), fontWeight: FontWeight.bold, fontSize: 24),)
-                        ],),
+                      child: FeesInfo(Color(0xffFCEEEE), 'Pending Fees', '2,244', Color(0xff9D0709)),
+                      height: 84, width: w/2 -22,
                     ),
                     //Green box
                     Container(
-                      height: 84,
-                      width: w/2-23,
-                      padding: EdgeInsets.only(top: 15, left: 12) ,
-                      decoration: BoxDecoration(
-                        color: Color(0xffE9EDEC),
-                        borderRadius: BorderRadius.circular(6),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text('Total collected', style: TextStyle(color: Color(0xff1C504B), fontSize: 14, fontWeight: FontWeight.w500),),
-                          SizedBox(height: 6,),
-                          Text('₹10,000', style: TextStyle(color: Color(0xff006C67), fontWeight: FontWeight.bold, fontSize: 24),)
-                        ],),
-                    ),
+                      height: 84, width: w/2 - 22,
+                      child: FeesInfo(Color(0xffE9EDEC), 'Total collected', '10,000', Color(0xff006C67)),
+                    )
                   ],),
               ),
 
               const SizedBox(height: 36,),
-
             ],
           ),
         ),
@@ -134,45 +107,10 @@ class _HomepageState extends State<Homepage> {
                     ],)),
               const SizedBox(height: 24,),
 
-              //Student info
-              Container(
-                  width: w,
-                  margin: EdgeInsets.only(left: 16, right: 16),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.4), spreadRadius: 1, blurRadius: 10)],
-                    borderRadius: BorderRadius.circular(6),
-                  ),
-                  child: Container(
-                    margin: const EdgeInsets.only(left: 16, right: 16, top: 20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text('Harshit Pathak', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),),
-                        const SizedBox(height: 8,),
-                        const Text('Physics', style: TextStyle(color: Colors.grey, fontSize: 16,),),
-                        const SizedBox(height: 12,),
-                        const Text('6/10 paid', style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),),
-                        const SizedBox(height: 8,),
-                        SvgPicture.asset('img/progress.svg'),
-                        const SizedBox(height: 26,),
-                        Container(height: 40,
-                          decoration: BoxDecoration(
-                            color: const Color(0xffE6F0F0),
-                            borderRadius: BorderRadius.circular(4),
-                          ),
-                          child: Center(
-                              child: RichText(text: TextSpan(children: [TextSpan(text: "View", style: TextStyle(color: Color(0xff09635E), fontSize: 16,)),WidgetSpan(child: Icon(Icons.arrow_forward, size: 16, color: Color(0xff09635E),),),],),)
-                          ),
-                        ),
-                        const SizedBox(height: 14,),
-                        Center(child: Text('Add Student', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 16),)),
-                        const SizedBox(height: 26,),
-                      ],
-                    ),
-                  )
-              )
+              //Students info
+
+
+
             ],
           ),
         )
@@ -181,3 +119,31 @@ class _HomepageState extends State<Homepage> {
   }
 
 }
+class FeesInfo extends StatelessWidget {
+
+  Color backgroundColor, textColor;
+  var title;
+  var pendingAmount;
+  FeesInfo(this.backgroundColor, this.title, this.pendingAmount, this.textColor);
+
+  @override
+  Widget build(BuildContext context) {
+    // var w = MediaQuery.of(context).size.width;
+    return Container(
+      padding: EdgeInsets.only(top: 15, left: 12) ,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title.toString(), style: TextStyle(color: textColor, fontSize: 14, fontWeight: FontWeight.w500),),
+          SizedBox(height: 6,),
+          Text(pendingAmount.toString(), style: TextStyle(color: textColor, fontWeight: FontWeight.bold, fontSize: 24),)
+        ],),
+    );
+  }
+}
+
